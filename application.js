6af70064315c88418c32bd31e00a2c9c5312b8cd
Zhusee Zhang <zhusee2@gmail.com>
2012-07-23 10:51:48 +0800

    Scroll to original position after resizing

--- application.js
+++ application.js
@@ -1,6 +1,8 @@
 window.addEventListener('resize', function(event) {
+  var currentScroll = {x: window.scrollX, y: window.scrollY};
   document.querySelector('#content').setAttribute('style', 'display: none;');
   setTimeout(function() {
     document.querySelector('#content').removeAttribute('style');
+    window.scrollTo(currentScroll.x, currentScroll.y)
   }, 0);
 });
