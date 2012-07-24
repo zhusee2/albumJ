4d5395017cad09fc6f09941986f60a0d3cab7318
Zhusee Zhang <zhusee2@gmail.com>
2012-07-25 01:44:55 +0800

    Modulize forceRedraw(); force redraw after load.

--- application.js
+++ application.js
@@ -1,19 +1,24 @@
-window.addEventListener('resize', function(event) {
+window.addEventListener('resize', foreceRedraw);
+window.addEventListener('load', foreceRedraw);
+
+function foreceRedraw() {
   var currentScroll = {x: window.scrollX, y: window.scrollY};
   document.querySelector('#content').setAttribute('style', 'display: none;');
   setTimeout(function() {
     document.querySelector('#content').removeAttribute('style');
     if (currentScroll.x != 0) window.scrollTo(currentScroll.x, currentScroll.y)
   }, 0);
-});
+}
 
 window.addEventListener('load', function(event) {
-  var boxes = document.querySelectorAll('.post');
-  
-  for (var i = 0; i < boxes.length; i++) {
-    var pic = boxes[i].querySelector('img'),
-        classString = boxes[i].getAttribute('class') + ' explicit';
+  setTimeout(function() {
+    var boxes = document.querySelectorAll('.post');
     
-    if (pic.offsetHeight > pic.offsetWidth) boxes[i].setAttribute('class', classString);
-  }
+    for (var i = 0; i < boxes.length; i++) {
+      var pic = boxes[i].querySelector('img'),
+          classString = boxes[i].getAttribute('class') + ' explicit';
+      
+      if (pic.offsetHeight > pic.offsetWidth) boxes[i].setAttribute('class', classString);
+    }
+  }, 50);
 });
